4eb441653d366d75c0f3cd203d5df5b1b281d886
ahqsa24 <adhiet353@gmail.com>
2025-10-21 14:20:47 +0700

    fix: favicon logo to implement in all routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Logo } from './content.js'
+
+function setFavicon(href) {
+  if (!href) return
+  let link = document.querySelector("link[rel*='icon']") || document.createElement('link')
+  link.type = 'image/png'
+  link.rel = 'icon'
+  link.href = href
+  document.getElementsByTagName('head')[0].appendChild(link)
+}
+
+const defaultLogo = Logo
+if (defaultLogo) setFavicon(defaultLogo)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
